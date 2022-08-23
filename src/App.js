9c8b45fef60efd6ab4806ccabaeafa64882e59da
Huyyyy <ongconoizzz@gmail.com>
2022-08-23 22:14:42 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <p className="font-bold text-4xl w-max">HUY PRO</p>
+      <button className="bg-blue-500 rounded w-36 text-white">
+        Follow me!
+      </button>
+    </div>
+  );
+}
+
+export default App;
